56e0eb39d5341350db2032c632563fc328b77690
worng1234 <54342155+worng1234@users.noreply.github.com>
2021-09-07 00:51:37 +0700

    fix file

--- app/Models/studentcoreModels.php
+++ app/Models/studentcoreModels.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class studentcoreModels extends Model
 {
+    use HasApiTokens, HasFactory, Notifiable;
+
     protected $table = "student_core";
     protected $fillable = [
         'student_id_card',
@@ -35,5 +39,24 @@ class studentcoreModels extends Model
         'name_cen',
         'surname',
         'rememberToken',
+    ];
+
+     /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'student_id',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 }
